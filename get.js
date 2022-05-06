e547cdfcbef83fc70e3ef97b1da77e6bb09c112d
anslem <chibuikeanselm@Chibuikes-MacBook-Pro.local>
2022-05-06 15:42:38 +0100

    fixed validation bug

--- get.js
+++ get.js
@@ -1,14 +1,13 @@
 const axios=require('axios');
 const env=require('dotenv');
-//const validate=require('./validation');
+const helpers=require('./validation');
 const url ='https://api.lloydsbank.com/open-banking/v2.2/branches'
 
 
 
 const fetchBranch = async (req,res)=>{
 
-    //const location=validate.validate(req.params.location);
-    //console.log(location);
+    const validated =helpers.validate(req.params.location);
     const option={
         headers:{
           "lbg-txn-branch-location":"London"
@@ -17,9 +16,12 @@ const fetchBranch = async (req,res)=>{
 
    console.log('started');
    try {
-    const data= await axios.get(url,option);
-    console.log(data.data || "no data returned");
-    res.send(data.data);
+     
+     if(validated){
+      const data= await axios.get(url,option);
+      console.log(data.data || "no data returned");
+      res.send(data.data);
+     }
    } catch (error) {
        console.log(error);
    }
--- validation.js
+++ validation.js
@@ -1,15 +1,16 @@
+
+
 const validate= (url)=>{
-  if(typeof(url) !== string){
-      return ('header must be a string value');
-  }
-  if(url==null){
-      return ('header must not be a null value');
-  }
-  else{
-      return {
-          status:true,
-          message:url
-      };
-  }
+    if((!typeof url === String)){ 
+        throw error;
+    }
+    if ((typeof url == null)){
+        throw error;
+    }
+    else{
+        return true;
+    }
 }
+
+
 module.exports ={validate}
