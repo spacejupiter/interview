5309f0c0b35c449a179c950f1380acb35be97f20
anslem <chibuikeanselm@Chibuikes-MacBook-Pro.local>
2022-05-06 14:48:46 +0100

    added validation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # interview
+#  curl http:localhost:5000/branches/parameter e.g location
+# use node server.js to start server
--- get.js
+++ get.js
@@ -1,14 +1,14 @@
 const axios=require('axios');
 const env=require('dotenv');
-const validate=require('./validation');
+//const validate=require('./validation');
 const url ='https://api.lloydsbank.com/open-banking/v2.2/branches'
 
 
 
 const fetchBranch = async (req,res)=>{
 
-    const location=validate.validate(req.params.location);
-    console.log(location);
+    //const location=validate.validate(req.params.location);
+    //console.log(location);
     const option={
         headers:{
           "lbg-txn-branch-location":"London"
