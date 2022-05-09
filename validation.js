03435eb4994edc433a6d89e85bd727cfb34ca213
anslem <chibuikeanselm@Chibuikes-MacBook-Pro.local>
2022-05-09 18:17:07 +0100

    validation added

--- validation.js
+++ validation.js
@@ -1,21 +1,21 @@
 
-
 const validate= (location)=>{
     try {
-        if((!typeof location === String)){ 
+        if((!typeof location === String)){
             return ('location must be a string value');
         }
         if ((typeof location == null || location == '')){
             return ('location must not be empty or null');
-            
+           
         }
         else{
             return true;
         }
     } catch (error) {
         return(error);
-    }  
-}
-
-
-module.exports ={validate}
+    } 
+ }
+ 
+ 
+ module.exports ={validate}
+ 
